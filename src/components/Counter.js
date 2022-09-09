960ae4be8c071c9101e3e4e558ffd6c8d30547c1
Rayhan Kabir <rayhan.kabir29@gmail.com>
2022-09-10 01:23:03 +0600

    useReducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,24 @@
-import React from "react";
+import { React, useReducer } from "react";
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 const Counter = () => {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <div>Count - 0</div>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <div>Count - {count}</div>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
     </div>
   );
 };
